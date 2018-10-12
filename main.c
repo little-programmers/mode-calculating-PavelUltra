2da770b1080a6651b8f23ea0070aece611edec88
PavelUltra <43610301+PavelUltra@users.noreply.github.com>
2018-10-12 22:45:03 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,34 @@
 
 // calculate_mode возвращает моду из массива а.
 // вместо точки с запятой открывайте фигурные скобки и пишите ваше решение.
-int calculate_mode(int n, int a[n]);
+int calculate_mode(int n, int a[n]){
+	int count1 = 0;
+	int count2 = 0;
+	int moda = a[0];
+	int std = 0;
+	int g = 0;
+	int j;
+	int i;
+	
+	for( j = 0; j < n; j++){
+		g = a[j];
+			for (i= 0;i<n;i++){
+				if(g == a[i])
+					count1 ++;
+			}	
+		
+		if(count1 > count2 ){
+			moda = a[j];
+			count2 = count1;
+		}
+		else if(count1 >= count2 && moda < a[j] ){
+			moda = a[j];
+			count2 = count1;	
+		}
+		count1 = 0;
+		}
+	return moda;
+}
 
 
 void test_calculate_mode() {
